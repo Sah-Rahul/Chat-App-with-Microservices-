aea328edc99087d2028984dcba794743931a5325
Sah-Rahul <sahfamilyinfo@gmail.com>
2026-01-13 15:47:51 +0545

    connect rabbitmq

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,9 +1,19 @@
 import express, {} from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import { createClient } from "redis";
+import userRouter from "./routes/user.routes.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+export const ConnectRedis = createClient({
+    url: process.env.REDIS_URL,
+});
+ConnectRedis.on("error", (err) => console.log("Redis Client Error", err));
+await ConnectRedis.connect();
+console.log("✅ Redis connected");
+connectRabbitMQ();
 // Middleware
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -12,6 +22,7 @@ app.get("/", (req, res) => {
         message: "Server is running 🚀",
     });
 });
+app.use("api/v1", userRouter);
 // Start Server
 const startServer = async () => {
     await connectDB();
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,25 @@
 import express, { type Request, type Response } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import { createClient } from "redis";
+import userRouter from "./routes/user.routes.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+export const ConnectRedis = createClient({
+  url: process.env.REDIS_URL as string,
+});
+
+ConnectRedis.on("error", (err) => console.log("Redis Client Error", err));
+
+await ConnectRedis.connect();
+console.log("✅ Redis connected");
+
+connectRabbitMQ();
 
 // Middleware
 app.use(express.json());
@@ -17,6 +31,7 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
+app.use("api/v1", userRouter);
 // Start Server
 const startServer = async () => {
   await connectDB();
